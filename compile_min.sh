1aa229be2bf87274bdad78cd65d456c3980141e9
Ethan Brierley <ethanboxx@gmail.com>
2019-05-06 21:24:42 +0100

    linux scripts

---
+++ compile_min.sh
@@ -0,0 +1,7 @@
+gnome-terminal --command="sass style/css: --style compressed"
+sleep 5
+gnome-terminal --command="pug index.pug"
+sleep 5
+gnome-terminal --command="image-watcher -c"
+sleep 5
+gnome-terminal --command="html-minifier index.html -o index.html --collapseBooleanAttributes --removeComments --preserveLineBreaks --minify-css true --minify-js true --maxLineLength 100"
---
+++ compile_watch.sh
@@ -0,0 +1,7 @@
+ggnome-terminal --command="sass style/css:"
+sleep 5
+gnome-terminal --command="sass --watch style/css:"
+sleep 5
+gnome-terminal --command="pug index.pug -w -P"
+sleep 5
+gnome-terminal --command="image-watcher -w"
